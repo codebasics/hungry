43cdad4066a44e4588686e2ea2c122077eb5ebd4
praveen1000 <36142111+praveen1000@users.noreply.github.com>
2018-02-17 00:28:50 -0500

    Codebasics Learner

--- hungry.py
+++ hungry.py
@@ -3,5 +3,6 @@ if hungry=="yes" and hungry=="Y":
     print("eat samosa")
     print("eat pizza")
     print("eat burger")
+    print ("I am working on this after watching your tutorial")
 else:
     print("do homework")    
