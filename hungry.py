c45530a917ad39aaf1aa5264cde2ad9b2a1f77bf
adrian-hodrea <45093870+adrian-hodrea@users.noreply.github.com>
2019-10-01 08:33:02 +0300

    added YES as a valid answer

--- hungry.py
+++ hungry.py
@@ -1,5 +1,5 @@
 hungry=input("are you hungry?")
-if hungry=="yes" and hungry=="Y" and hungry=="y":
+if hungry=="yes" hungry=="YES" and hungry=="Y" and hungry=="y":
     print("eat samosa")
     print("eat pizza")
     print("eat burger")
