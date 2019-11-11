aa8887a433bd0f05bec5c217e94d548ecec0059c
vishu chhabra <55664689+vishuchhabra@users.noreply.github.com>
2019-11-11 22:19:26 +0530

    Update hungry.py
    added this into your code

--- hungry.py
+++ hungry.py
@@ -3,5 +3,7 @@ if hungry=="yes" and hungry=="Y" and hungry=="y":
     print("eat samosa")
     print("eat pizza")
     print("eat burger")
+    print("eat dinner");
+    
 else:
     print("do homework")    
