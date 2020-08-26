334bec625d21c9c229f6478804d9fa306bae087f
Divyosmi Goswami <goswamidivyosmi@gmail.com>
2020-08-26 13:44:28 +0530

    Create Hungry.py
    Change from and to or operator.

---
+++ Hungry.py
@@ -0,0 +1,13 @@
+hungry=input("are you hungry?")
+
+if hungry=="yes" or hungry=="Y" or hungry=="y" or hungry == "Yes":
+
+    print("eat samosa")
+
+    print("eat pizza")
+
+    print("eat burger")
+
+else:
+
+    print("do homework")    
