8a4823f4bfe8458efb83deea0bedcd5e5c355f58
Divyosmi Goswami <goswamidivyosmi@gmail.com>
2020-09-15 14:22:43 +0530

    Update hungry.py

--- hungry.py
+++ hungry.py
@@ -1,7 +1,7 @@
 hungry=input("are you hungry?")
 if hungry=="yes" or hungry=="Y" or hungry=="y" or hungry == "Yes":
-    print("eat samosa")
-    print("eat pizza")
-    print("eat burger")
+    print("eat samosa😋 ")
+    print("eat pizza 🍕")
+    print("eat burger 🍔")
 else:
     print("do homework")    
