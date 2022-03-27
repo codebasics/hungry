7bbde7b8013accbb0efa828bb0a33437f2ba0209
elric1993 <62893435+elric1993@users.noreply.github.com>
2022-03-27 11:48:30 +0800

    Added elif maybe as a valid answer

--- hungry.py
+++ hungry.py
@@ -3,5 +3,7 @@ if hungry=="yes" and hungry=="Y" and hungry=="y":
     print("eat samosa")
     print("eat pizza")
     print("eat burger")
+elif hungry=="maybe" and hungry=="m" and hungry=="M":
+    print("drink water")
 else:
     print("do homework")    
