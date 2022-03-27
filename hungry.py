7e8f4db7ca302a5878d042496a3a4a258a617ca4
elric1993 <62893435+elric1993@users.noreply.github.com>
2022-03-27 13:42:16 +0800

    Update hungry.py

--- hungry.py
+++ hungry.py
@@ -3,7 +3,7 @@ if hungry=="yes" and hungry=="Y" and hungry=="y":
     print("eat samosa")
     print("eat pizza")
     print("eat burger")
-elif hungry=="maybe" and hungry=="m" and hungry=="M":
+elif hungry=="maybe" or hungry=="m" or hungry=="M":
     print("drink water")
 else:
     print("do homework")    
