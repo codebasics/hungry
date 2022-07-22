30123b6fcfd137f3f4fefd283b1c2a9a5b13a94a
Narendr9 <109784564+Narendr9@users.noreply.github.com>
2022-07-22 15:46:22 +0530

    Update hungry.py
    Nested if placed f

--- hungry.py
+++ hungry.py
@@ -1,7 +1,9 @@
 hungry=input("are you hungry?")
 if hungry=="yes" and hungry=="Y" and hungry=="y":
+    if hungry=="y" :
     print("eat samosa")
     print("eat pizza")
+    else:
     print("eat burger")
 else:
     print("do homework")    
