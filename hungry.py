2a1ec1502e39f2c95e6a21e41bebfdeca4af851f
8mansi <76641107+8mansi@users.noreply.github.com>
2023-04-13 21:11:26 +0530

    added hi

--- hungry.py
+++ hungry.py
@@ -3,5 +3,6 @@ if hungry=="yes" and hungry=="Y" and hungry=="y":
     print("eat samosa")
     print("eat pizza")
     print("eat burger")
+    print("Hiiiiiiiii")
 else:
     print("do homework")    
